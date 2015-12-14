a083b122de3859a72aa51cf7922b6ea644772ad2
Pheophorbide <daria.paramonova@gmail.com>
2015-12-15 00:33:07 +0300

    Module8 - task1 -  Форма: оживление числовых полей

---
+++ js/map.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+  ymaps.ready(init);
+  var myMap,
+    myPlacemark;
+
+  function init(){
+    myMap = new ymaps.Map("map", {
+      center: [59.94, 30.32],
+      zoom: 15
+    });
+
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/map-marker.svg',
+      // Размеры метки.
+      iconImageSize: [35.983, 35.983],
+      iconImageOffset: [-3, -42]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+  }
+};
