5fe9e085ccbddd6e3c24315f67ef49a3a448fe57
Pheophorbide <daria.paramonova@gmail.com>
2015-12-17 10:53:04 +0300

    Module9-task1 - Оживляем гамбургер в личном проекте

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,20 @@
 (function() {
-  var menuBlock = document.querySelector(".js-mainMenu");
-  console.log(menuBlock);
+  var rowOpen = document.querySelector(".js-rowOpen");
+  var menuBlock = rowOpen.querySelector(".js-mainMenu");
   var toggle = menuBlock.querySelector(".toggle");
   console.log(toggle);
 
   var toggleOpen = menuBlock.querySelector(".toggle").querySelector(".toggle__burger");
   var toggleClose = menuBlock.querySelector(".toggle").querySelector(".toggle__close");
-  var menu = menuBlock.querySelector(".header-menu__menu-inner");
+
+  console.log(rowOpen);
 
   toggleOpen.onclick = function () {
-    menuBlock.classList.toggle("open");
+    /*menuBlock.classList.toggle("open");*/
+    rowOpen.classList.toggle("open");
   };
   toggleClose.onclick = function () {
-    menuBlock.classList.remove("open");
+    rowOpen.classList.remove("open");
   };
 
 })();
