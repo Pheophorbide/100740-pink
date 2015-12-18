efa12eef4738b4d971eeb2ae5832838865e0ec4e
Pheophorbide <daria.paramonova@gmail.com>
2015-12-19 00:51:46 +0300

    Оживляем гамбургер в личном проекте - исправила меню

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,8 @@
 (function() {
   var rowOpen = document.querySelector(".js-rowOpen");
-  var menuBlock = rowOpen.querySelector(".js-mainMenu");
-  var toggle = menuBlock.querySelector(".toggle");
-  
-  var toggleOpen = menuBlock.querySelector(".toggle").querySelector(".toggle__burger");
-  var toggleClose = menuBlock.querySelector(".toggle").querySelector(".toggle__close");
-
-  toggleOpen.onclick = function () {
-    /*menuBlock.classList.toggle("open");*/
+  var toggle = rowOpen.querySelector(".toggle");
+  toggle.addEventListener("click",function (event) {
+    event.preventDefault();
     rowOpen.classList.toggle("open");
-  };
-  toggleClose.onclick = function () {
-    rowOpen.classList.remove("open");
-  };
-
+  });
 })();
