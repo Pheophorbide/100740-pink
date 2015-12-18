53e0663ca0a537eec39ce99f6b2a260921c2e7fe
Pheophorbide <daria.paramonova@gmail.com>
2015-12-18 18:27:16 +0300

    Perfect Pixel форма

--- js/menu.js
+++ js/menu.js
@@ -2,12 +2,9 @@
   var rowOpen = document.querySelector(".js-rowOpen");
   var menuBlock = rowOpen.querySelector(".js-mainMenu");
   var toggle = menuBlock.querySelector(".toggle");
-  console.log(toggle);
-
+  
   var toggleOpen = menuBlock.querySelector(".toggle").querySelector(".toggle__burger");
   var toggleClose = menuBlock.querySelector(".toggle").querySelector(".toggle__close");
-
-  console.log(rowOpen);
 
   toggleOpen.onclick = function () {
     /*menuBlock.classList.toggle("open");*/
