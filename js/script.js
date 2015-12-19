b7dcf27bd62dfb460f1d3435140dba04555f2121
Pheophorbide <daria.paramonova@gmail.com>
2015-12-19 23:48:16 +0300

    Форма: доработка числовых полей

--- js/script.js
+++ js/script.js
@@ -35,13 +35,21 @@
 
     minus.addEventListener("click", function () {
       changeNumber("minus");
+      var evt = document.createEvent("HTMLEvents");
+      evt.initEvent("change", false, true);
+      input.dispatchEvent(evt);
     });
 
     plus.addEventListener("click", function () {
       changeNumber("plus");
+      var evt = document.createEvent("HTMLEvents");
+      evt.initEvent("change", false, true);
+      input.dispatchEvent(evt);
     });
 
-    function changeNumber(operation) {
+
+
+      function changeNumber(operation) {
       var value = parseDaysValue(input.value);
       if (operation == "plus" && value < 100500) {
         input.value = (value + 1) + " " + declOfNum((value + 1), input.getAttribute("data-decl").split(","));
@@ -60,4 +68,62 @@
     var cases = [2, 0, 1, 1, 1, 2];
     return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
   }
+
+  var inputFellow = document.querySelector(".js-fellow");
+  var inputValue = parseDaysValue(inputFellow.value);
+
+  for (var i= 0; i < inputValue; i++) {
+    addFellowContainer();
+  }
+
+  inputFellow.addEventListener("change", function(event){
+    var currentValue = parseDaysValue(event.target.value);
+    var currentContainerCount = document.querySelectorAll(".js-fellowItemContainer").length;
+    console.log(currentValue, currentContainerCount);
+    if (currentContainerCount < currentValue) {
+      addFellowContainer();
+    } else if (currentContainerCount > currentValue) {
+      removeLastFellowContainer();
+    }
+  });
+  function addFellowContainer () {
+    var fellowInner = document.querySelector(".js-fellowInner");
+    var count = document.querySelectorAll('.js-fellowItemContainer').length + 1;
+    var div = document.createElement("div");
+    div.classList.add("js-fellowItemContainer");
+    div.innerHTML = getFellowItemHTML(count);
+    fellowInner.appendChild(div);
+  }
+
+  function removeLastFellowContainer () {
+    var lastFellowContainer = document.querySelector(".js-fellowItemContainer:last-child");
+    lastFellowContainer.remove();
+  }
+
+  function getFellowItemHTML (indexNumber) {
+    return '<div class="form-container__form form-container__form--wrapper">' +
+    '<div class="form-container__form  form-container__form--fellow-inner">' +
+    '<div class="form-container__fellow-number">' +
+    '<div class="form-container__label  form-container__label--fellow-number">№</div>' +
+    '<div class="form-container__control  form-container__control--fellow-number">' + indexNumber + '</div>' +
+    '</div>' +
+    '<div class="form-container__fellow-name">' +
+    '<div class="form-container__label form-container__label--important"><label' +
+    'for="fellow-name-1">Имя:</label></div>' +
+    '<div class="form-container__control  form-container__control--fellow-count">' +
+    '<input type="text" id="fellow-name-1" placeholder="Введите Ваше имя" name="fellow-name-1">' +
+    '</div>' +
+    '</div>' +
+    '<div class="form-container__delete"><a href="#">Удалить</a></div>' +
+    '<div class="form-container__fellow-name  form-container__fellow-name--margin-left">' +
+    '<div class="form-container__label"><label for="fellow-nickname-1">Прозвище:</label></div>' +
+    '<div class="form-container__control  form-container__control--fellow-count">' +
+    '<input type="text" id="fellow-nickname-1" placeholder="Ну как же без этого"' +
+    'name="fellow-nickname-1">' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div> ';
+  }
+
 })();
