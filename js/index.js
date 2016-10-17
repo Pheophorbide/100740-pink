d9fa8ca6d4b3949ddb45aee211f13cd2aebc9a13
Daria <daria.paramonova@gmail.com>
2016-10-17 19:12:39 +0300

    Добавила недостающие стили

---
+++ js/index.js
@@ -0,0 +1,33 @@
+(function() {
+  var rowOpen = document.querySelector(".js-rowOpen");
+  var toggle = rowOpen.querySelector(".toggle");
+  toggle.addEventListener("click",function (event) {
+    event.preventDefault();
+    rowOpen.classList.toggle("open");
+  });
+})();
+
+( function () {
+  ymaps.ready(init);
+  var myMap,
+    myPlacemark;
+
+  function init(){
+    myMap = new ymaps.Map("map", {
+      center: [59.94, 30.32],
+      zoom: 15
+    });
+
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+
+    }, {
+      iconLayout: "default#image",
+      iconImageHref: "img/map-marker.svg",
+      // Размеры метки.
+      iconImageSize: [35.983, 35.983],
+      iconImageOffset: [-3, -42]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+  }
+}) ();
